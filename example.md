eea71921f38f9643b68569664cc3343bfe7a4910
BimaAdi <bimaadi419@gmail.com>
2026-09-12 09:20:25 +0700

    Ready, Set, Go

---
+++ example.md
@@ -0,0 +1,35 @@
+# GFM
+
+## Autolink literals
+
+www.example.com, https://example.com, and contact@example.com.
+
+## Footnote
+
+A note[^1]
+
+[^1]: Big note.
+
+## Strikethrough
+
+~one~ or ~~two~~ tildes.
+
+## Table
+
+| a | b  |  c |  d  |
+| - | :- | -: | :-: |
+| item 1 | item 2 | item 3 | item 4 |
+
+## Tasklist
+
+* [ ] to do
+* [x] done
+
+## Mermaid
+```mermaid
+flowchart LR
+    A[Start] --> B{Is it working?}
+    B -- Yes --> C[Great!]
+    B -- No --> D[Debug Code]
+    D --> B
+```
---
+++ index.ts
@@ -0,0 +1,59 @@
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+
+const parseMdtoHtml = async ({ filePath }: { filePath: string }) => {
+	const inputFile = Bun.file(filePath);
+	const text = await inputFile.text();
+
+	const file = await unified()
+		.use(remarkParse)
+		.use(remarkGfm)
+		.use(remarkRehype)
+		.use(rehypeStringify)
+		.process(text);
+
+	return String(file);
+};
+
+const server = Bun.serve({
+	routes: {
+		"/styles.css": async () => {
+			const cssFile = Bun.file("./styles.css");
+			return new Response(await cssFile.text(), {
+				headers: {
+					"Content-Type": "text/css; charset=utf-8",
+				},
+			});
+		},
+
+		"/": async () => {
+			const rawBody = await parseMdtoHtml({ filePath: "example.md" });
+			const rawHtml = `<html>
+      <head>
+      <link rel="stylesheet" href="styles.css">
+      <style>
+        .content {
+          margin: auto;
+        }
+      </style>
+      </head>
+      <body>
+      <div class="content">
+      ${rawBody}
+      </div>
+      </body>
+      </html>`;
+			return new Response(rawHtml, {
+				status: 200,
+				headers: {
+					"content-type": "text/html; charset=utf-8",
+				},
+			});
+		},
+	},
+});
+
+console.log(`server run on ${server.port}`);
